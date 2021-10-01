9c786152b0943aa60be1ec1de8b2f0628c7bec60
Olrik Heuser <post@olrikheuser.de>
2021-10-01 15:10:44 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,3 +1,44 @@
 # CodableObservable
 
-A description of this package.
+This Package aims to make the ObservableObject type comply to the Codable Protocol.
+:warning: It depends on some Mirror Magic and has to be regarded highly unstable :bangbang:
+
+## Installation
+
+### Swift Package Manager (SPM)
+
+URL:
+
+`https://github.com/resinnovation/CodableObservable.git`
+
+Manifest:
+
+```swift
+dependencies: [
+    .package(url: "https://github.com/resinnovation/CodableObservable.git", .upToNextMajor(from: "0.1.0" )),
+]
+```
+
+## Usage
+
+After importing the following should be possible:
+
+```swift
+class CodableObservabeClass: ObservableObject, Codable {
+    @Published var string = "aString"
+    @Published var int = 69
+}
+```
+
+The Package adds conformance of Encodable/Decodable to Published properties as long as the corressponding value itsself is Encodable/Decodable
+
+## Credits
+
+The implementation builds on the following answer:  
+https://stackoverflow.com/a/59918094
+
+## License 
+
+[CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/)
+
+
--- Sources/CodableObservable/CodableObservable.swift
+++ Sources/CodableObservable/CodableObservable.swift
@@ -1,6 +1,37 @@
-public struct CodableObservable {
-    public private(set) var text = "Hello, World!"
+import Combine
 
-    public init() {
+extension Published: Encodable where Value: Encodable {
+
+    private var valueChild:Value? {
+            let mirror = Mirror(reflecting: self)
+            if let valueChild = mirror.descendant("value") as? Value {
+                return valueChild
+            }
+
+            //iOS 14 does things differently...
+            if let valueChild = mirror.descendant("storage","value") as? Value {
+                return valueChild
+            }
+
+            //iOS 14 does this too...
+            if let valueChild = mirror.descendant("storage","publisher","subject","currentValue") as? Value {
+                return valueChild
+            }
+
+            return nil
+        }
+
+    public func encode(to encoder: Encoder) throws {
+        guard let valueChild = valueChild else {
+            fatalError("Could not read Value")
+        }
+        try valueChild.encode(to: encoder)
+    }
+}
+
+extension Published: Decodable where Value: Decodable {
+    public init(from decoder: Decoder) throws {
+        let value = try Value.init(from: decoder)
+        self.init(initialValue: value)
     }
 }
